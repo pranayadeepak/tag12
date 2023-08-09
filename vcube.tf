bd9ec43a5e9408ed22a9737ec331c16e1a43f7e8
pranayadeepak <139688827+pranayadeepak@users.noreply.github.com>
2023-08-09 20:53:01 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region     = "eu-north-1"
-  access_key = "AKIA4EMLMLTME6U6ZPVP"
-  secret_key = "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"
+  access_key = "AKIA4EMLMLTMCY6CSQ5Y"
+  secret_key = "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"
 }
 
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-deepak-s3-chintha-s3-madhu"
+  bucket = "s3-pranay-s3-omgsiva-s3-layeeqh"
 
   acl    = "private"   # or can be "public-read"
 
