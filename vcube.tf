ea90c12da10967dcb6992acc034cc4efb789858d
pranayadeepak <139688827+pranayadeepak@users.noreply.github.com>
2023-08-10 06:34:19 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-pranay-s3-omgsiva-s3-layeeqh1234562000179"
+  bucket = "s3-pranay-s3-omgsiva-s3-layeeqh1234562000199"
 
   acl    = "private"   # or can be "public-read"
 
@@ -22,8 +22,8 @@ resource "aws_s3_bucket" "b1" {
 }
 # Upload an object
 resource "aws_s3_bucket_object" "file" {
-bucket   = "s3-pranay-s3-omgsiva-s3-layeeqh1234562000179"
-key      = "tom.pem"
+bucket   = "s3-pranay-s3-omgsiva-s3-layeeqh1234562000199"
+key      = "deepak.txt"
 acl    = "private"   # or can be "public-read"
-source = "C:/Users/LENOVO/Downloads/tom.pem"
+source = "/Users/LENOVO/Downloads/deepak.txt"
 } 
