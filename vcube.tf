c74dae5d72c26cb72a6362da829bd52f8753397c
pranayadeepak <139688827+pranayadeepak@users.noreply.github.com>
2023-08-10 11:33:41 +0530

    Update vcube.tf

--- vcube.tf
+++ vcube.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Create a bucket
 resource "aws_s3_bucket" "b1" {
 
-  bucket = "s3-pranay-s3-omgsiva-s3-layeeqh12345620003032"
+  bucket = "s3-pranay-s3-omgsiva-s3-layeeqh123456200030378"
 
   acl    = "private"   # or can be "public-read"
 
@@ -21,8 +21,8 @@ resource "aws_s3_bucket" "b1" {
 
 }
 # Upload an object
-resource "aws_s3_bucket_object" "file.txt" {
-bucket   = "s3-pranay-s3-omgsiva-s3-layeeqh12345620003032"
+resource "aws_s3_bucket_object" "file" {
+bucket   = "s3-pranay-s3-omgsiva-s3-layeeqh123456200030378"
 key      = "deepu.txt"
 acl    = "private"   # or can be "public-read"
 source = "/Users/LENOVO/Documents/deepu.txt"
